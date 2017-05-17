ac85451ade0beeec3c297093fa6c85dfec4e0487
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-17 13:13:21 +0100

    code cleanup

--- server/config/routes.js
+++ server/config/routes.js
@@ -53,8 +53,9 @@ module.exports = (app,passport)=>{
 
 function isLoggedIn(req, res, next) {
     // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
+    if (req.isAuthenticated()){
         return next();
+    }
     // if they aren't redirect them to the home page
     res.redirect('/');
 }

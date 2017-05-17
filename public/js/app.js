e283879ae63f350e23c851f9f8d73af70e6a367f
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-17 11:20:05 +0100

    added logic to force to http

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+    $(function() {
+        if (window.location.protocol === "https:")
+            window.location.protocol = "http";
+    });
+};
+
 var currentUser = (document.getElementById("user").innerHTML);
 
 angular.module('App',['ngSanitize'])

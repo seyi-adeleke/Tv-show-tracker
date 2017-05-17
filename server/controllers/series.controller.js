b30f5144694e5c66a51decac418b6f03ec6fe162
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-17 10:36:14 +0100

    created series controllers and functions

---
+++ server/controllers/series.controller.js
@@ -0,0 +1,29 @@
+const Series = require("../models/series.models");
+const Users = require("../models/users.models");
+
+exports.list = function(req, res) {
+    Series.find({user:req.user.firstName}).sort().limit(12).exec(function(err,series){
+        res.render('favourites',{series: series, user : req.user})
+    })
+};
+
+exports.createFav = function(req,res) {
+    var entry = new Series({
+        user: req.user.firstName,
+        name: req.body.name,
+        premierDate: req.body.premierDate,
+        status: req.body.status
+    });
+
+    entry.save(function (err) {
+        if (err) {
+            var errMsg = 'Sorry, there was an error saving your favorite. ' + err;
+            res.render('/', {user: req.user, firstName: req.body.firstName, message: req.flash(errMsg)});
+        }
+        else {
+            console.log('Your favourite was saved correctly');
+            res.redirect('/favourites');
+        }
+    });
+
+};

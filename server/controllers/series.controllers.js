428d3bbaf9f91b796e0cfc456af1fc9cdca53e12
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-19 06:48:48 +0100

    fixed series controller bug

--- server/controllers/series.controllers.js
+++ server/controllers/series.controllers.js
@@ -2,14 +2,14 @@ const Series = require("../models/series.models");
 const Users = require("../models/users.models");
 
 exports.list = function(req, res) {
-    Series.find({user:req.user.firstName}).sort().limit(12).exec(function(err,series){
+    Series.find({user:req.user.firstName}).sort().limit(30).exec(function(err,series){
         res.render('favourites',{series: series, user : req.user})
     })
 };
 
 exports.createFav = function(req,res) {
     let entry = new Series({
-        user: req.user.firstName,
+        user: req.body.user,
         name: req.body.name,
         premierDate: req.body.premierDate,
         status: req.body.status
@@ -20,8 +20,7 @@ exports.createFav = function(req,res) {
             res.render('/', {user: req.user, firstName: req.body.firstName, message: req.flash(errMsg)});
         }
         else{
-            console.log('Your favourite was saved correctly');
-            res.redirect('/favourites');
+             console.log('Your favourite was saved correctly');
         }
     });
 };
